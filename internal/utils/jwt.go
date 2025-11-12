ada925b12c06b2c03ca8907ba775dcad1e019481
liuxingyu <yuwen002@163.com>
2025-11-13 00:33:40 +0800

    chore(deps): 更新并添加go-zero及JWT依赖
    - 在go.mod中添加github.com/zeromicro/go-zero依赖
    - 引入github.com/golang-jwt/jwt/v5版本依赖及相关间接依赖
    - 添加jwt.go工具，新增生成JWT令牌函数GenerateToken
    - 使用jwt v5版本实现JWT签发，支持payload和过期时间配置

--- internal/utils/jwt.go
+++ internal/utils/jwt.go
@@ -1,1 +1,18 @@
 package utils
+
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func GenerateToken(secret string, expire int64, payload map[string]interface{}) (string, error) {
+	claims := jwt.MapClaims{}
+	for k, v := range payload {
+		claims[k] = v
+	}
+	claims["exp"] = time.Now().Add(time.Second * time.Duration(expire)).Unix()
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(secret))
+}
